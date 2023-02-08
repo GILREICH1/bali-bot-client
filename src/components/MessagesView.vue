017f40e058be0c156868f4609d98a5f047f2ef87
gilreich1 <gilreich12@gmail.com>
2023-02-08 20:30:01 +0200

    initial commit

---
+++ src/components/MessagesView.vue
@@ -0,0 +1,31 @@
+<template>
+  <template :key="i" v-for="(message, i) in messages">
+    <n-card>
+      {{ message }}
+      <template #footer>
+        {{ "footer" }}
+      </template>
+    </n-card>
+  </template>
+</template>
+
+<script>
+import { NCard } from "naive-ui";
+
+export default {
+  components: {
+    NCard
+  },
+  data() {
+    return {
+      messages: ["hello", "test"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.n-card {
+  max-width: 300px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <TheWelcome />
+    <n-space vertical>
+      <n-input round placeholder="Hello">
+        <template #prefix>
+          {{ "👋" }}
+        </template>
+      </n-input>
+    </n-space>
+  </main>
+</template>
+
+<script>
+import TheWelcome from "../components/MessagesView.vue";
+import { NSpace, NInput, NIcon } from "naive-ui";
+
+export default {
+  components: {
+    NSpace,
+    NIcon,
+    NInput,
+    TheWelcome,
+  },
+};
+</script>
